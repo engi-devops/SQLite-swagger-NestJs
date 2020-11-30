d137026901d3ea98a98f994cf8b246b73d998747
santosh singh <singhsantosh.edu.in@gmail.com>
2020-11-30 18:43:08 +0530

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
-async function bootstrap() {
+async function nestJsSQLiteApplication() {
   const app = await NestFactory.create(AppModule);
+  
   // handle all user input validation globally
   app.useGlobalPipes(new ValidateInputPipe());
-  await app.listen(3000);
+  const port = 3000;
+
+  await app.listen(port, () =>
+    console.log(
+      `Nest Js with sequelize SQLite App listening on port ${port} !`,
+    ),
+  );
 }
-bootstrap();
+nestJsSQLiteApplication();
