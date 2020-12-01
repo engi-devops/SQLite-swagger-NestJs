5bf2b7c010a0c283db4d310189617351465c5a53
manoranjannayak <manoranjannayak08@gmail.com>
2020-12-01 12:26:23 +0530

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import swaggerDoc from './swaggerDoc/index.json';
 
 async function nestJsSQLiteApplication() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,14 @@ async function nestJsSQLiteApplication() {
   // handle all user input validation globally
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-
+  const options = new DocumentBuilder()
+  .setTitle('Users')
+  .setDescription('The users API description')
+  .setVersion('1.0')
+  .addTag('users')
+  .build();
+const document = SwaggerModule.createDocument(app, options);
+SwaggerModule.setup('api', app, swaggerDoc );
   const port = 3000;
 
   await app.listen(port, () =>
