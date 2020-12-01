f660dbc5e7d093f699ba026bd6769dff88f47d3c
santosh singh <singhsantosh.edu.in@gmail.com>
2020-12-01 08:22:02 +0530

    initial commit

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,3 @@
-// import { FileInterceptor, FilesInterceptor, FileFieldsInterceptor } from '@nestjs/platform-express';
-// import { diskStorage } from 'multer';
-
-// export const fileStorage =  FileInterceptor('image',{
-//     storage: diskStorage({
-//         destination: './upload',
-//         filename:(req,file,cb)=>{
-//             return cb(null,file.originalname);
-//         }
-//     }),
-// })
-
 import { extname } from 'path';
 
 export const imageFileFilter = (req, file, callback) => {
